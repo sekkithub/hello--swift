1db9eab5eab9d37cbf60eb473cdc1cd7c52269e1
Masato Seki <masatoseki@the-neighbourhood.com>
2015-05-19 17:50:28 +0100

    Dieting multiple functions

--- Hello--Swift/ViewController.swift
+++ Hello--Swift/ViewController.swift
@@ -34,27 +34,26 @@ class ViewController: UIViewController {
         audioPlayer01 = AVAudioPlayer(contentsOfURL: Sound01, error: &error)
         audioPlayer01.prepareToPlay()
         audioPlayer01.play()
-        audioPlayer01.volume = 1.0;
+        audioPlayer01.volume = 1.0
         audioPlayer01.numberOfLoops = -1
         
         audioPlayer02 = AVAudioPlayer(contentsOfURL: Sound02, error: &error)
         audioPlayer02.prepareToPlay()
         audioPlayer02.play()
-        audioPlayer02.volume = 0;
+        audioPlayer02.volume = 0
         audioPlayer02.numberOfLoops = -1
     }
 
-    func fadePlayer01(player: AVAudioPlayer,
-        fromVolume startVolume : Float,
+    func fadePlayer(player: AVAudioPlayer,
         toVolume endVolume : Float,
         overTime time : Float) {
             
-            // Update the volume every 1/100 of a second
+            // Update the volume every 1/10 of a second
             var fadeSteps : Int = Int(time) * 10
             // Work out how much time each step will take
             var timePerStep : Float = 1 / 100.0
-            
-            self.audioPlayer01.volume = startVolume;
+        
+            var startVolume = player.volume
             
             // Schedule a number of volume changes
             for step in 0...fadeSteps {
@@ -67,57 +66,25 @@ class ViewController: UIViewController {
                     
                     let fraction = (Float(step) / Float(fadeSteps))
                     
-                    self.audioPlayer01.volume = startVolume +
+                    player.volume = startVolume +
                         (endVolume - startVolume) * fraction
                 }
             }
     }
-
-    func fadePlayer02(player: AVAudioPlayer,
-        fromVolume startVolume : Float,
-        toVolume endVolume : Float,
-        overTime time : Float) {
-            
-            // Update the volume every 1/100 of a second
-            var fadeSteps : Int = Int(time) * 10
-            // Work out how much time each step will take
-            var timePerStep : Float = 1 / 100.0
-            
-            self.audioPlayer02.volume = startVolume;
-            
-            // Schedule a number of volume changes
-            for step in 0...fadeSteps {
-                
-                let delayInSeconds : Float = Float(step) * timePerStep
-                
-                let popTime = dispatch_time(DISPATCH_TIME_NOW,
-                    Int64(delayInSeconds * Float(NSEC_PER_SEC)));
-                dispatch_after(popTime, dispatch_get_main_queue()) {
-                    
-                    let fraction = (Float(step) / Float(fadeSteps))
-                    
-                    self.audioPlayer02.volume = startVolume +
-                        (endVolume - startVolume) * fraction
-                }
-            }
-    }
-
-    
 
     override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+        let country = audioPlayer01.volume
+        let metal = audioPlayer02.volume
 
-        if UIDevice.currentDevice().orientation == .LandscapeLeft {
-            println("Landscape Left")
-            fadePlayer01(audioPlayer01, fromVolume: 1.0, toVolume: 0.0, overTime: 50.0)
-            fadePlayer02(audioPlayer02, fromVolume: 0.0, toVolume: 1.0, overTime: 50.0)
-        } else if UIDevice.currentDevice().orientation == .LandscapeRight {
-            println("Landscape Right")
-            fadePlayer01(audioPlayer01, fromVolume: 1.0, toVolume: 0.0, overTime: 50.0)
-            fadePlayer02(audioPlayer02, fromVolume: 0.0, toVolume: 1.0, overTime: 50.0)
+        if UIDevice.currentDevice().orientation == .Portrait {
+            fadePlayer(audioPlayer01, toVolume: 1.0, overTime: 50.0)
+            fadePlayer(audioPlayer02, toVolume: 0.0, overTime: 50.0)
+            println("portrait")
         } else {
-            println("Portrait")
-            fadePlayer01(audioPlayer01, fromVolume: 0.0, toVolume: 1.0, overTime: 50.0)
-            fadePlayer02(audioPlayer02, fromVolume: 1.0, toVolume: 0.0, overTime: 50.0)
+            fadePlayer(audioPlayer01, toVolume: 0.0, overTime: 50.0)
+            fadePlayer(audioPlayer02, toVolume: 1.0, overTime: 50.0)
+            println("landscape")
+
         }
     }
 }
